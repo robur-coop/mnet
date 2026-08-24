1e90079b7f13d6c2190c42bc1684bda338efe2fc
Hannes Mehnert <hannes@mehnert.org>
2026-08-24 14:19:28 +0200

    move happy eyeballs command line arguments to mnet-happy-eyeballs

--- src/mnet_cli.mli
+++ src/mnet_cli.mli
@@ -5,7 +5,6 @@ open Cmdliner
 
 val s_network : Cmdliner.Manpage.section_name
 val s_dns : Cmdliner.Manpage.section_name
-val s_happy_eyeballs : Cmdliner.Manpage.section_name
 val s_output : Cmdliner.Manpage.section_name
 val s_logs : Cmdliner.Manpage.section_name
 
@@ -42,18 +41,6 @@ val setup_nameservers :
 (** [setup_nameservers] aggregates {!val:nameservers} and verify if all of them
     use the same protocol (see {!type:Dns.proto}) or not. It fails if one of the
     given nameserver uses a different protocol from the others. *)
-
-type happy_eyeballs = {
-    aaaa_timeout: int64
-  ; connect_delay: int64
-  ; connect_timeout: int64
-  ; resolve_timeout: int64
-  ; resolve_retries: int
-}
-
-val setup_happy_eyeballs : happy_eyeballs Term.t
-(** [setup_happy_eyeballs] aggregates options to configure an Happy Eyeballs
-    instance. *)
 
 val setup_logs : bool Term.t
 (** [setup_logs] setups logs. *)
--- src/mnet_happy_eyeballs.mli
+++ src/mnet_happy_eyeballs.mli
@@ -127,3 +127,15 @@ val connect :
     See {!val:connect_host} for parameter descriptions. *)
 
 val inject : t -> getaddrinfo -> unit
+(** [inject t getaddrinfo] mutates [t] to use the provided [getaddrinfo]. *)
+
+type setup = {
+    aaaa_timeout: int64
+  ; connect_delay: int64
+  ; connect_timeout: int64
+  ; resolve_timeout: int64
+  ; resolve_retries: int
+}
+
+val setup : setup Cmdliner.Term.t
+(** [setup] aggregates options to configure an Happy Eyeballs instance. *)
